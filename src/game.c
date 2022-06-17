844f308cb4464d32d7f55fd2132c6280a636e41e
athersoft <athersoft>
2022-06-17 19:51:10 -0400

    Integrado el movimiento del jugador

--- src/game.c
+++ src/game.c
@@ -8,27 +8,33 @@ square *createSquare(){
     strcpy(Square -> type, "vacio");
     Square -> symbol = '0';
     Square -> colision = false;
+    Square -> isText = false;
+    Square -> text = (char *) malloc(sizeof(char)*10);
+    strcpy(Square -> text, "");
     return Square;
 }
 
 lvl *createLvl(){
     lvl *Lvl = (lvl*) malloc(sizeof(lvl));
-    Lvl -> height = 100;
-    Lvl -> width = 100;
-    Lvl -> posx = 50;
-    Lvl -> posy = 99;
-    for (int i = 0; i<100; i++){
-        for (int j = 0; j<100; j++){
+    Lvl -> height = 50;
+    Lvl -> width = 50;
+    Lvl -> posx = 25;
+    Lvl -> posy = 25;
+    for (int i = 0; i<50; i++){
+        for (int j = 0; j<50; j++){
             Lvl -> map[i][j] = createSquare();
         }
     }
+    strcpy(Lvl -> map[Lvl -> posx][Lvl -> posy]->type, "Player");
     Lvl -> map[Lvl -> posx][Lvl -> posy]->symbol = 'J';
+    Lvl -> map[Lvl -> posx][Lvl -> posy]->colision = true;
     return Lvl;
 }
 
 void showLvl(lvl *Lvl){
-    for(int i = Lvl->posy-5; i<Lvl -> posy+5; i++){
-        for(int j = Lvl->posx-3; j<Lvl->posx+3; j++){
+    /*
+    for(int i = Lvl->posy-4; i<Lvl -> posy+4; i++){
+        for(int j = Lvl->posx-7; j<Lvl->posx+7; j++){
             if(i >= Lvl -> height){
                 break;
             }
@@ -36,13 +42,20 @@ void showLvl(lvl *Lvl){
         }
         printf("\n");
     }
+    */
+    for(int i = 0; i < Lvl -> height; i++){
+        for(int j = 0; j < Lvl -> width; j++){
+            printf("%c", Lvl -> map[i][j] -> symbol);
+        }
+        printf("\n");
+    }
 }
 
 int movementX(char in){
     switch (in){
-        case 'A':
+        case 'a':
             return -1;
-        case 'D':
+        case 'd':
             return 1;
     }       
     return 0;
@@ -50,10 +63,10 @@ int movementX(char in){
 
 int movementY(char in){
     switch (in){
-        case 'W':
+        case 'w':
             return -1;
 
-        case 'S':
+        case 's':
             return 1;
 
     }       
@@ -67,13 +80,15 @@ void updateLvl(lvl *Lvl){
     getchar();
     clrscr();
 
-    if(Lvl -> map[Lvl -> posx + movementX(in)][Lvl -> posy + movementY(in)] -> colision == false){
-        Lvl -> map[Lvl -> posx + movementX(in)][Lvl -> posy + movementY(in)] = Lvl -> map[Lvl -> posx][Lvl ->posy];
-        Lvl -> map[Lvl ->posx][Lvl ->posy]->symbol = 0;
+    if(Lvl -> map[Lvl -> posy + movementY(in)][Lvl -> posx + movementX(in)] -> colision == false){
+        Lvl -> map[Lvl -> posy + movementY(in)][Lvl -> posx + movementX(in)] -> symbol = 'J';
+        Lvl -> map[Lvl ->posy][Lvl ->posx]->symbol = '0';
         Lvl -> posx += movementX(in);
         Lvl -> posy += movementY(in);
     }
 
     showLvl(Lvl);
-    updateLvl(Lvl);
+    if(in != '0'){
+        updateLvl(Lvl);
+    }
 }
